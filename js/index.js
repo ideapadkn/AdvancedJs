af2b8c2aaab2fd99e798cb107f6e138420c83023
Kamron or Камрон <kamrush6429@gmail.com>
2023-09-20 11:15:02 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// ====================================
+
+const innerContainer = document.querySelector(".inner");
+const btn = document.querySelector(".btn");
+const api = "https://jsonplaceholder.typicode.com/users";
+
+
